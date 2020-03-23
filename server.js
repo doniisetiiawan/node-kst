826d30bf1ba248a4179d6c36197e8084e5a52008
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-23 11:59:44 +0700

    ...

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ import connect from 'connect';
 
 const app = connect();
 
+// eslint-disable-next-line no-unused-vars
+const helloWorld = (req, res, next) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  res.end('Hello World');
+};
+
+app.use(helloWorld);
+
 app.listen(3000);
 
 console.log('Server running at http://localhost:3000/');
