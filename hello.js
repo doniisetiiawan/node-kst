a78898e0d91f47d4d103d72d64d40f6f5dcff006
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-23 11:44:26 +0700

    ...

--- hello.js
+++ hello.js
@@ -1,10 +1,5 @@
-// Invoke 'strict' JavaScript mode
-// Define a module variable
-const message = 'Hello';
+export default () => {
+  const message = 'Hello';
 
-// Print message to the console
-function sayHello() {
   console.log(message);
-}
-
-export default sayHello;
+};
--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// Invoke 'strict' JavaScript mode
-// Load the 'hello' module
 import sayHello from './hello';
 
-// Use the 'hello' module sayHello() method
 sayHello();
