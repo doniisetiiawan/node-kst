3a4be51fa12be247e0eeb9ac425853372fc47dc5
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-23 12:01:24 +0700

    ...

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ import connect from 'connect';
 
 const app = connect();
 
+const logger = (req, res, next) => {
+  console.log(req.method, req.url);
+
+  next();
+};
+
 // eslint-disable-next-line no-unused-vars
 const helloWorld = (req, res, next) => {
   res.setHeader('Content-Type', 'text/plain');
 
   res.end('Hello World');
 };
+
+app.use(logger);
 
 app.use(helloWorld);
 
