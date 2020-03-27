9ba2982f4a9d4fea9fd63a41ea91ebe661559324
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-27 12:34:59 +0700

    Installing Socket.io

--- config/express.js
+++ config/express.js
@@ -6,11 +6,17 @@ import methodOverride from 'method-override';
 import session from 'express-session';
 import passport from 'passport';
 import flash from 'connect-flash';
+import http from 'http';
+import socketio from 'socket.io';
+import MongoStorex from 'connect-mongo';
 
 import config from './config';
 import routes from '../app/routes/index.server.routes';
 import users from '../app/routes/users.server.routes';
 import articles from '../app/routes/articles.server.routes';
+import socketiox from './socketio';
+
+const MongoStore = MongoStorex(session);
 
 export default () => {
   const app = express();
@@ -29,11 +35,18 @@ export default () => {
   app.use(bodyParser.json());
   app.use(methodOverride());
 
-  app.use(session({
-    saveUninitialized: true,
-    resave: true,
-    secret: config.sessionSecret,
-  }));
+  const mongoStore = new MongoStore({
+    url: config.db,
+  });
+
+  app.use(
+    session({
+      saveUninitialized: true,
+      resave: true,
+      secret: config.sessionSecret,
+      store: mongoStore,
+    }),
+  );
 
   app.use(flash());
   app.use(passport.initialize());
@@ -45,5 +58,10 @@ export default () => {
 
   app.use(express.static('./public'));
 
-  return app;
+  const server = http.createServer(app);
+  const io = socketio.listen(server);
+
+  socketiox(server, io, mongoStore);
+
+  return server;
 };
---
+++ config/socketio.js
@@ -0,0 +1,34 @@
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import config from './config';
+
+export default (server, io, mongoStore) => {
+  io.use((socket, next) => {
+    cookieParser(config.sessionSecret)(
+      socket.request,
+      {},
+      (err) => {
+        const sessionId = socket.request.signedCookies['connect.sid'];
+
+        mongoStore.get(sessionId, (err, session) => {
+          socket.request.session = session;
+
+          passport.initialize()(socket.request, {}, () => {
+            passport.session()(socket.request, {}, () => {
+              if (socket.request.user) {
+                next(null, true);
+              } else {
+                next(
+                  new Error('User is not authenticated'),
+                  false,
+                );
+              }
+            });
+          });
+        });
+      },
+    );
+  });
+
+  io.on('connection', (socket) => {});
+};
